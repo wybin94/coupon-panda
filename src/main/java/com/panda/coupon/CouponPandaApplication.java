7251fcc2f7c0fd636cb3319df621cabb8849c4cc
won <wybin94@naver.com>
2024-08-26 17:25:24 +0900

    first commit

---
+++ src/main/java/com/panda/coupon/CouponPandaApplication.java
@@ -0,0 +1,13 @@
+package com.panda.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponPandaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponPandaApplication.class, args);
+    }
+
+}
