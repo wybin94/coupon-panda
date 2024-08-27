8268607c249b41d0ad0ee98744abccf61e0f2822
won <wybin94@naver.com>
2024-08-27 17:41:42 +0900

    Revert "first commit"
    This reverts commit 7251fcc2

---
+++ src/test/java/com/panda/coupon/CouponPandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panda.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponPandaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
